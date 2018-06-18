4991ad9cc650dda7aee78f05a0342d7056ceda41
dhrandy <dhrandy@gmail.com>
2018-06-18 18:26:50 -0400

    initial upload

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+// create the methods that will execute the necessary MySQL commands in the controller. These are the methods you will need to use in order to retrieve and store data in your database.
+// - selectAll()
+// - insertOne()
+// - updateOne()
+
+//export the ORM object in module.exports
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,2 @@
+// import Express and burger.js
+// 1. Create the router for the app, and export the router at the end of your file.
