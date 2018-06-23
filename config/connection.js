1f70eeec13708518b8a4a30ea78f7cb340c6a759
dhrandy <dhrandy@gmail.com>
2018-06-23 19:52:52 -0400

    added heroku mysql info

--- config/connection.js
+++ config/connection.js
@@ -20,6 +20,17 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
 
+// Export connection for our ORM to use
+connection.connect();
 module.exports = connection;
